872f0e60f34bfef91c6cc837544bd3bebba3dd38
Bruno Passos <brpassos.programador@gmail.com>
2017-01-05 19:15:22 -0200

    map - teste leaflet

--- resources/views/conexoes/js.blade.php
+++ resources/views/conexoes/js.blade.php
@@ -257,3 +257,159 @@ print_r($periodo_limite);*/
         angular.element('#serieCtrl').scope().$apply();
     }
 </script>
+
+
+
+@if($rota=='map')
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.2/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet@1.0.2/dist/leaflet.js"></script>
+    <script>
+        var map;
+        autocomplete = [];
+
+        $(document).ready(initialize);
+
+        function initialize(){
+            $("#map").height($(window).height());
+
+            map = L.map("map", {
+                center: L.latLng(-20, -45),
+                zoom: 5
+            });
+
+            //var tileLayer = L.tileLayer("http://{s}.acetate.geoiq.com/tiles/acetate/{z}/{x}/{y}.png").addTo(map);
+            var tileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYnJwYXNzb3MiLCJhIjoiY2l4N3l0bXF0MDFiczJ6cnNwODN3cHJidiJ9.qnfh8Jfn_be6gpo774j_nQ', {
+                maxZoom: 18,
+                attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+                '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+                id: 'mapbox.streets'
+            }).addTo(map);
+
+
+
+
+            //next: add features to map
+            getData();
+        };
+
+        /*var mymap = L.map('mapid').setView([-16, -53], 4);
+         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYnJwYXNzb3MiLCJhIjoiY2l4N3l0bXF0MDFiczJ6cnNwODN3cHJidiJ9.qnfh8Jfn_be6gpo774j_nQ', {
+         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+         maxZoom: 18,
+         id: 'mapbox.streets'
+         }).addTo(mymap);*/
+
+        function mapData(data){
+            //remove existing map layers
+            map.eachLayer(function(layer){
+                //if not the tile layer
+                if (typeof layer._url === "undefined"){
+                    map.removeLayer(layer);
+                }
+            });
+
+            //create geojson container object
+            var geojson = {
+                "type": "FeatureCollection",
+                "features": []
+            };
+
+            //areas
+            //mapArea('');
+
+            //split data into features
+            var dataArray = data.split(", ;");
+            dataArray.pop();
+
+            //console.log(dataArray);
+
+            //build geojson features
+            dataArray.forEach(function(d){
+                d = d.split(", "); //split the data up into individual attribute values and the geometry
+
+                //feature object container
+                var feature = {
+                    "type": "Feature",
+                    "properties": {}, //properties object container
+                    "geometry": JSON.parse(d[fields.length]) //parse geometry
+                };
+
+                for (var i=0; i<fields.length; i++){
+                    feature.properties[fields[i]] = d[i];
+                };
+
+                //add feature names to autocomplete list
+                if ($.inArray(feature.properties.featname, autocomplete) == -1){
+                    autocomplete.push(feature.properties.featname);
+                };
+
+                geojson.features.push(feature);
+            });
+
+            //console.log(geojson);
+
+            //activate autocomplete on featname input
+            //$("input[name=featname]").autocomplete({
+                //source: autocomplete
+            //});
+
+            var mapDataLayer = L.geoJson(geojson, {
+                pointToLayer: function (feature, latlng) {
+                    var markerStyle = {
+                        fillColor: "#CC9900",
+                        color: "#FFF",
+                        fillOpacity: 0.5,
+                        opacity: 0.8,
+                        weight: 1,
+                        radius: 8
+                    };
+
+                    return L.circleMarker(latlng, markerStyle);
+                },
+                onEachFeature: function (feature, layer) {
+                    var html = "";
+                    for (prop in feature.properties){
+                        html += prop+": "+feature.properties[prop]+"<br>";
+                    };
+                    layer.bindPopup(html);
+                }
+            }).addTo(map);
+        }
+
+        function getData(){
+            $.ajax("get-data", {
+                data: {
+                    table: "ed_territorios_uf"
+                },
+                success: function(data){
+                    console.log(data);
+                    mapData(data);
+                }
+            })
+        }
+
+        function getColor(d) {
+            return d > 1000 ? '#800026' :
+                d > 500  ? '#BD0026' :
+                    d > 200  ? '#E31A1C' :
+                        d > 100  ? '#FC4E2A' :
+                            d > 50   ? '#FD8D3C' :
+                                d > 20   ? '#FEB24C' :
+                                    d > 10   ? '#FED976' :
+                                        '#FFEDA0';
+        }
+
+        function style(feature) {
+            return {
+                fillColor: getColor(feature.properties.density),
+                weight: 2,
+                opacity: 1,
+                color: 'white',
+                dashArray: '3',
+                fillOpacity: 0.7
+            };
+        }
+
+    </script>
+@endif
