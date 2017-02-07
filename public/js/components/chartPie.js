056f63e399874b03de21a46223d71221f7f41a9d
Bruno Passos <brpassos.programador@gmail.com>
2017-02-07 14:16:06 -0200

    charts

--- public/js/components/chartPie.js
+++ public/js/components/chartPie.js
@@ -93,14 +93,14 @@ class ChartPie extends React.Component {
     }
 
     getColors(values) {
-        if (this.state.intervalos.length > 0) {
-            let colors = [];
-            for (let i in values) {
-                colors.push(convertHex(getColor(values[i], intervalos), 100));
-            }
-            console.log('chartPie', colors);
-            return colors;
+        //if(this.state.intervalos.length > 0){
+        let colors = [];
+        for (let i in values) {
+            colors.push(convertHex(getColor(values[i], intervalos), 100));
         }
+        console.log('chartPie', colors);
+        return colors;
+        //}
     }
 
     render() {
