f946aa07965735a5cca027819a842b5ed6d65e03
Bruno Passos <brpassos.programador@gmail.com>
2017-04-20 22:13:29 -0300

    quem somos

--- app/Http/Controllers/QuemController.php
+++ app/Http/Controllers/QuemController.php
@@ -12,15 +12,11 @@ use Illuminate\Support\Facades\Log;
 
 class QuemController extends Controller
 {
-    public function detalhar($id=null){
+    public function detalhar(){
 
         $lang =  App::getLocale();
 
-        $quem = DB::table('quemsomos')->where('idioma_sigla', $lang)->where('id', $id)->orderBy('titulo')->first();
-        if($id){
-            $quem = DB::table('quemsomos')->where('idioma_sigla', $lang)->orderBy('titulo')->first();
-        }
-
+        $quem = DB::table('quemsomos')->where('idioma_sigla', $lang)->orderBy('titulo')->first();
         $menus = DB::table('quemsomos')->where('idioma_sigla', $lang)->where('tipo', 1)->where('origem_id', 1)->orderBy('titulo')->get();
 
         return view('quem.detalhar', ['quem' => $quem, 'menus' => $menus]);
