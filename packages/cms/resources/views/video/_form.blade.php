3ae2518a2a30690ef2eb85b2f0e7299852baf515
Relison Galvao <relison@cd10.com.br>
2017-05-23 17:29:16 -0300

    Video

--- packages/cms/resources/views/video/_form.blade.php
+++ packages/cms/resources/views/video/_form.blade.php
@@ -3,8 +3,8 @@
 {!! Form::label('titulo', 'Título *') !!}<br>
 {!! Form::text('titulo', null, ['class'=>"form-control width-grande <% validar(video.titulo) %>", 'ng-model'=>'video.titulo', 'ng-required'=>'true', 'init-model'=>'video.titulo', 'placeholder' => '']) !!}<br>
 
-{!! Form::label('data', 'Data ') !!}<br>
-{!! Form::text('data', null, ['class'=>"form-control width-grande <% validar(video.data) %>", 'ng-model'=>'video.data', 'ng-required'=>'true', 'init-model'=>'video.data', 'placeholder' => '']) !!}<br>
+{!! Form::label('data', 'Data ') !!} 00/00/0000<br>
+{!! Form::text('data', null, ['class'=>"form-control width-medio <% validar(video.data) %>", 'ng-model'=>'video.data', 'ng-required'=>'true', 'init-model'=>'video.data', 'placeholder' => '']) !!}<br>
 
 {!! Form::label('link_video', 'Video (link do youtube)') !!}<br>
 {!! Form::text('link_video', null, ['class'=>"form-control width-grande <% validar(video.link_video) %>", 'ng-model'=>'video.link_video', 'init-model'=>'video.link_video', 'placeholder' => '']) !!}<br>
