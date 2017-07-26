3c72356476b4c253b8726a72c7bda7b42fbe04e7
Bruno Passos <brpassos.programador@gmail.com>
2017-07-26 09:10:28 -0300

    migrations

---
+++ database/migrations/2017_07_26_120122_create_indicadores_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIndicadoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('indicadores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('titulo');
+            $table->string('slug')->nullable();
+            $table->integer('cmsuser_id')->unsigned();
+            $table->foreign('cmsuser_id')->references('id')->on('cms_users')->onDelete('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('indicadores');
+    }
+}
