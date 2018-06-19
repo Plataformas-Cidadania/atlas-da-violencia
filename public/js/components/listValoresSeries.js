68ee9376dece819e6e2284b723f8f1b88dcc0a30
Bruno Passos <brpassos.programador@gmail.com>
2018-06-19 19:58:22 -0300

    trocar abrangencia

--- public/js/components/listValoresSeries.js
+++ public/js/components/listValoresSeries.js
@@ -211,12 +211,7 @@ class ListValoresSeries extends React.Component {
             null,
             React.createElement(
                 'div',
-                { style: { display: this.state.loading || !this.state.dataTable ? '' : 'none' }, className: 'text-center' },
-                React.createElement('i', { className: 'fa fa-spin fa-spinner fa-4x' })
-            ),
-            React.createElement(
-                'div',
-                { style: { display: this.state.loading || !this.state.dataTable ? 'none' : '' } },
+                { style: { display: this.state.loading || !this.state.dataTable ? '' : '' } },
                 React.createElement(
                     'div',
                     { className: 'Container' },
