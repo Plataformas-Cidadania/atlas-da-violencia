41efcb5278f03f2261403d88dda43de9d54436b6
Bruno Passos <brpassos.programador@gmail.com>
2018-07-03 17:41:58 -0300

    bug tipo regiao corrigido

--- app/Http/Controllers/SerieController.php
+++ app/Http/Controllers/SerieController.php
@@ -508,7 +508,7 @@ class SerieController extends Controller
                 ['valores_series.serie_id', $id],
                 ['valores_series.periodo', '>=', $min],
                 ['valores_series.periodo', '<=', $max],
-                ['valores_series.tipo_regiao', '<=', $abrangencia]
+                ['valores_series.tipo_regiao', '=', $abrangencia]
             ])
             ->whereIn("$tabelas[$abrangencia].edterritorios_codigo", $regions)
             ->orderBy('valores_series.periodo')
