a2162fbf921e21bba3f4e61b7851e344bcf09798
Bruno Passos <brpassos.programador@gmail.com>
2018-07-10 18:08:44 -0300

    id_seq

--- packages/cms/resources/views/id_seq.blade.php
+++ packages/cms/resources/views/id_seq.blade.php
@@ -5,12 +5,14 @@
             <th>tabela</th>
             <th>id_seq</th>
             <th>ultimo id</th>
+            <th>status</th>
         </tr>
         @foreach($rows as $row)
             <tr>
                 <td>{{$row[0]}}</td>
                 <td>{{$row[1]}}</td>
                 <td>{{$row[2]}}</td>
+                <td>@if($row[1] >= $row[2]) OK @else erro @endif</td>
             </tr>
         @endforeach
     </table>
