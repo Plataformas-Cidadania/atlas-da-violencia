c5f1de135a88d61b038a2c3c9e4baf424956a367
Bruno Passos <brpassos.programador@gmail.com>
2018-07-10 18:04:39 -0300

    id_seq

--- packages/cms/resources/views/id_seq.blade.php
+++ packages/cms/resources/views/id_seq.blade.php
@@ -1,17 +1,17 @@
 
 <div style="margin:auto; width: 600px;">
     <table>
-        {{--@foreach($rows as $row)
+        <tr>
+            <th>tabela</th>
+            <th>id_seq</th>
+            <th>ultimo id</th>
+        </tr>
+        @foreach($rows as $row)
             <tr>
-                <td>{{$row[0]->table}}</td>
-                <td>{{$row[0]->last_value}}</td>
+                <td>{{$row[0]}}</td>
+                <td>{{$row[1]}}</td>
+                <td>{{$row[2]}}</td>
             </tr>
-        @endforeach--}}
+        @endforeach
     </table>
 </div>
-
-@foreach($rows as $row)
-    <pre>
-        <?php print_r($row)?>;
-    </pre>
-@endforeach
