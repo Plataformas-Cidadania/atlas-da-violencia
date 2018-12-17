fae0ba799e792863094868540313e53d542cefdf
Bruno Passos <brpassos.programador@gmail.com>
2018-12-17 17:50:06 -0200

    acertos

--- resources/assets/js/components/chartLine.js
+++ resources/assets/js/components/chartLine.js
@@ -138,10 +138,16 @@ class ChartLine extends React.Component{
 
             for(let periodo in data[region]){
                 values.push(data[region][periodo]);
-                if(cont==0){
+                /*if(cont==0){
                     labels[contLabel] = formatPeriodicidade(periodo, this.props.periodicidade);
                     contLabel++;
+                }*/
+                let per = formatPeriodicidade(periodo, this.props.periodicidade);
+                if(!labels.includes(per)){
+                    labels[contLabel] = per;
+                    contLabel++;
                 }
+
             }
 
             //console.log('values', values);
