1eb9ed479cfe7fce64a09c19847d02053f7eb5ca
Bruno Passos <brpassos.programador@gmail.com>
2018-12-31 17:23:36 -0200

    acerto class migration

--- database/migrations/2018_12_20_163723_alter_temas_table.php
+++ database/migrations/2018_12_20_163723_alter_temas_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterTemasTable extends Migration
+class AlterTemasTable2 extends Migration
 {
     /**
      * Run the migrations.
