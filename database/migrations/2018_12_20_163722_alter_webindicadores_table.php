d8131e6d9ed00acd493ffdf9a883f136d935a770
Bruno Passos <brpassos.programador@gmail.com>
2018-12-31 17:18:33 -0200

    acerto class migration

--- database/migrations/2018_12_20_163722_alter_webindicadores_table.php
+++ database/migrations/2018_12_20_163722_alter_webindicadores_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterWebindicadoresTable extends Migration
+class AlterWebindicadoresTable2 extends Migration
 {
     /**
      * Run the migrations.
