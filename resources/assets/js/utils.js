fb16e1e79b4dd387a3b14be16a4a875c7c55e04d
Bruno Passos <brpassos.programador@gmail.com>
2019-01-11 11:47:03 -0200

    legenda

--- resources/assets/js/utils.js
+++ resources/assets/js/utils.js
@@ -179,7 +179,7 @@ function getColor(d, intervalos) {
 
     var colors = ['#4285F4', '#689DF6', '#8EB6F8', '#B3CEFB', '#F6D473', '#F1B567', '#ED965B', '#E87850',  '#E45A45',  '#E0433C'];
 
-
+    console.log(intervalos);
     var qtdIntervalos = intervalos.length;
     for(var i=qtdIntervalos-1; i>=0; i--){
         console.log('d/intervalos', d, intervalos[i]);
