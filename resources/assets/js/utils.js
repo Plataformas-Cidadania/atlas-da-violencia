ffcd64971ead9c170540edc813981cc8142160bc
Bruno Passos <brpassos.programador@gmail.com>
2019-01-11 12:00:32 -0200

    legenda

--- resources/assets/js/utils.js
+++ resources/assets/js/utils.js
@@ -181,9 +181,8 @@ function getColor(d, intervalos) {
 
     console.log(intervalos);
     var qtdIntervalos = intervalos.length;
-    //for(var i=qtdIntervalos-1; i>=0; i--){
-    for(var i=0; i<=qtdIntervalos-1; i++){
-        console.log('d/intervalos', d, intervalos[i]);
+    for(var i=qtdIntervalos-1; i>=0; i--){
+        console.log('d/i/intervalos', i, d, intervalos[i]);
         if(d > intervalos[i]){
             console.log('dentro if', colors[i]);
             return colors[i];
