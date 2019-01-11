a2010a186dcf2996e4ba8465b98ec861b3d0e37e
Bruno Passos <brpassos.programador@gmail.com>
2019-01-11 12:21:24 -0200

    home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,7 +28,7 @@ class HomeController extends Controller
 
         $noticias = DB::table('noticias')->where('idioma_sigla', $lang)->orderBy('id', 'desc')->take(3)->get();
         $ultimaPostagem = DB::table('artigos')->where('idioma_sigla', $lang)->orderBy('id', 'desc')->take(1)->first();
-        if(count($ultimaPostagem) == 0){
+        if(!empty($ultimaPostagem)){
             $ultimaPostagem = DB::table('noticias')->where('idioma_sigla', $lang)->orderBy('id', 'desc')->take(1)->first();
             $noticias = DB::table('noticias')->where('idioma_sigla', $lang)->orderBy('id', 'desc')->skip(1)->take(3)->get();
         }
