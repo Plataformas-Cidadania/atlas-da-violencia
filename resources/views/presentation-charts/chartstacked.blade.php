172455e2ba0c9995d70d1cbb6fcd4c8fb67d6459
Relison Galvão <relison@cd10.com.br>
2020-05-11 15:35:43 -0300

    Img Mapa RO

--- resources/views/presentation-charts/chartstacked.blade.php
+++ resources/views/presentation-charts/chartstacked.blade.php
@@ -113,4 +113,9 @@
 
     chart<?php echo $random;?>.render();
 
+    function numberToReal(numero) {
+        var numero = numero.toFixed(2).split('.');
+        numero[0] = "R$ " + numero[0].split(/(?=(?:...)*$)/).join('.');
+        return numero.join(',');
+    }
 </script>
