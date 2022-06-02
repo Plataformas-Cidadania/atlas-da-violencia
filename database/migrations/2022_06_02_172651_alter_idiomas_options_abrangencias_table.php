daff6c416a20858f5222c31815f640360233938a
Bruno Passos <brpassos.programador@gmail.com>
2022-06-02 17:27:54 -0300

    acertos

---
+++ database/migrations/2022_06_02_172651_alter_idiomas_options_abrangencias_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterIdiomasOptionsAbrangenciasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('idiomas_options_abrangencias', function (Blueprint $table) {
+            $table->foreign('option_abrangencia_id')->references('id')->on('options_abrangencias')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('idiomas_options_abrangencias', function (Blueprint $table) {
+            $table->foreign('option_abrangencia_id')->references('id')->on('options_abrangencias')->onDelete('cascade');
+        });
+    }
+}
