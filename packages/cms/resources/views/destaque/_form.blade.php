7f0b105f741130afe7bc92c6e2a5c4ffcc4e0cc7
Bruno Passos <brpassos.programador@gmail.com>
2022-08-16 23:39:12 -0300

    ADV-105

---
+++ packages/cms/resources/views/destaque/_form.blade.php
@@ -0,0 +1,18 @@
+{{--É NECESSÁRIO RODAR O COMANDO composer require illuminate/html E ALTERAR ACRESCENTAR LINHA NO ARQUIVO config/app.php--}}
+
+{!! Form::label('idioma_sigla', 'Idioma *') !!}<br>
+{!! Form::select('idioma_sigla',
+        $idiomas,
+null, ['class'=>"form-control width-medio <% validar(destaque.idioma_sigla) %>", 'ng-model'=>'destaque.idioma_sigla', 'ng-required'=>'true', 'init-model'=>'destaque.idioma_sigla', 'placeholder' => 'Selecione']) !!}<br>
+
+{!! Form::label('titulo', 'Título *') !!}<br>
+{!! Form::text('titulo', null, ['class'=>"form-control width-grande <% validar(destaque.titulo) %>", 'ng-model'=>'destaque.titulo', 'ng-required'=>'true', 'init-model'=>'destaque.titulo', 'placeholder' => '']) !!}<br>
+
+{!! Form::label('link', 'Link ') !!}<br>
+{!! Form::text('link', null, ['class'=>"form-control width-grande <% validar(destaque.link) %>", 'ng-model'=>'destaque.link', 'init-model'=>'destaque.link', 'placeholder' => '']) !!}<br>
+
+{!! Form::label('chamada', 'Chamada') !!}<br>
+{!! Form::textarea('descricao', null, ['class'=>"form-control width-grande <% validar(destaque.descricao) %>", 'ui-tinymce'=>'tinymceOptions', 'ng-model'=>'destaque.descricao', 'init-model'=>'destaque.descricao']) !!}<br>
+
+
+
